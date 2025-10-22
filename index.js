a9999a005cd0c7fbcdde11e56faf12e7ffed93cc
cedrc-pr <cedric.prin@gmail.com>
2025-10-22 12:26:09 +0200

    cleaned for TD 3

--- index.js
+++
@@ -1,6 +0,0 @@
-import * as lolcat from "lolcatjs";
-
-lolcat.options.seed = Math.round(Math.random() * 1000);
-lolcat.options.colors = true;
-
-lolcat.fromFile(process.argv[1]);
--- src/repeat.ts
+++
@@ -1,8 +0,0 @@
-export function repeat(text: string, times: number): string {
-  const arr: string[] = [];
-  while (times) {
-    arr.push(text);
-    times -= 1;
-  }
-  return arr.join("");
-}
