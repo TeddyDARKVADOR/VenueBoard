5a52ef1705d200002440d114e8a7c1afc5e74bee
cedrc-pr <cedric.prin@gmail.com>
2025-10-30 16:10:11 +0100

    add Zod

--- src/models.ts
+++ src/models.ts
@@ -1,9 +1,16 @@
-export type Operation = {
-  id: number;
-  kind: "ADD" | "SUB" | "MUL" | "DIV" | "MOD";
-  rhs: number;
-  lhs: number;
-  res: number;
-};
+import { z } from "zod";
 
-export type KindAndOperands = Omit<Omit<Operation, "id">, "res">;
+export const ZOperation = z.object({
+  id: z.coerce.number().positive().int(),
+  kind: z.enum(["ADD", "SUB", "MUL", "DIV", "MOD"]),
+  rhs: z.coerce.number(),
+  lhs: z.coerce.number(),
+  res: z.number(),
+});
+
+export const ZKindAndOperands = ZOperation.omit({ id: true, res: true });
+export const ZOperationId = ZOperation.pick({ id: true });
+
+export type Operation = z.infer<typeof ZOperation>;
+export type KindAndOperands = z.infer<typeof ZKindAndOperands>;
+export type OperationId = z.infer<typeof ZOperationId>;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,29 @@
 import Fastify from "fastify";
-import type { KindAndOperands } from "./models.js";
+import {
+  type OperationId,
+  ZKindAndOperands,
+  ZOperationId,
+  type KindAndOperands,
+} from "./models.js";
 import { OpeRepo } from "./Operation.js";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
 
 function start_web_server() {
-  const web_server = Fastify({ logger: true });
+  const web_server = Fastify({
+    logger: true,
+  }).withTypeProvider<ZodTypeProvider>();
+  web_server.setValidatorCompiler(validatorCompiler);
+  web_server.setSerializerCompiler(serializerCompiler);
+
   const ope = new OpeRepo();
 
   web_server.post<{ Body: KindAndOperands }>(
     "/operations",
+    { schema: { body: ZKindAndOperands } },
     async (req, res) => {
       const id = ope.createOperation(req.body);
       res.code(201);
@@ -19,10 +35,11 @@ function start_web_server() {
     return ope.readAllOperation();
   });
 
-  web_server.get<{ Params: { id: number } }>(
+  web_server.get<{ Params: OperationId }>(
     "/operations/:id",
+    { schema: { params: ZOperationId } },
     async (req, res) => {
-      const operation = ope.readOperationById(Number(req.params.id));
+      const operation = ope.readOperationById(req.params.id);
       if (!operation) {
         res.code(404);
         return { message: "Not found" };
